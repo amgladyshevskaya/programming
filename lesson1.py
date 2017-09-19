c97754aa971471aa4b1c688abfc4eac047e933c9
Студент НИУ ВШЭ <student@basm-402-std24.staff.corp.local>
2017-09-19 11:56:08 +0300

    lesson 1

---
+++ lesson1.py
@@ -0,0 +1,17 @@
+print ("Hello!")
+#x =10
+#y = x*100
+#print( 'y =',y)
+#print( 'x =', x, ', y =',y)
+
+#human_name = input('Как вас зовут? ')
+#print('Привет', human_name)
+#print(type(human_name))
+#a = int(input('введите число a: '))
+#b = int(input('введите число b: '))
+
+#print(a*b)
+a = int(input('введите число a: '))
+if a > 5:
+    print('Ваше число больше пяти')
+
