7c996e3106a2b9b7fa7945d22db25dc23a5003ca
amgladyshevskaya <31654743+amgladyshevskaya@users.noreply.github.com>
2017-11-29 19:54:10 +0300

    Add files via upload

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,8 +1,8 @@
 #Вариант3
 i=0
-word=str(input())
+word=str(input("Введите слово: "))
 with open("text.txt","w",encoding="utf-8") as f:
     while word!="":
         f.write(word[i+1:len(word)]+"\n")
-        word=str(input())
+        word=str(input("Введите слово: "))
         i=i+1
