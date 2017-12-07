0ce95198b69dcb03c876471139b58ceebe6c9ded
amgladyshevskaya <31654743+amgladyshevskaya@users.noreply.github.com>
2017-12-07 12:08:15 +0300

    Add files via upload

--- test/GladyshevskayaTest.py
+++ test/GladyshevskayaTest.py
@@ -1,5 +1,6 @@
 #Вариант2
-
+#Задание1
+"""
 with open("freq.txt", encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines:
@@ -7,3 +8,33 @@ with open("freq.txt", encoding="utf-8") as f:
         if cells[2] == "гл":
             if cells[4] == "перех":
                 print(line)
+"""
+#Задание2
+"""
+with open("freq.txt", encoding="utf-8") as f:
+    lines = f.readlines()
+    sum = 0
+    for line in lines:
+        cells = line.split()
+        if cells[2] == "част":
+            print(cells[0], end = ",")
+            sum += float(cells[4])
+print()
+print(sum)
+"""
+#Задание3
+words = []
+n = 0
+word = input("Введите слово: ")
+while word != "":
+    word = input("Введите слово: ")
+    n+=1
+with open("freq.txt", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        cells = line.split()
+    for i in range(n+1):
+        if cells[0] == word:
+            print(word, "IPM = ", cells[-1])
+        
+
